ce98b5c0a2db3356feebfb4d816b81c914a08c9b
dwang11 <dwang11@uoguelph.ca>
2023-03-27 18:25:16 -0400

    added basic react testing

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import sys;
 import io;
+import os
 import MolDisplay
 from http.server import HTTPServer, BaseHTTPRequestHandler;
 
@@ -9,6 +10,7 @@ class Handler(BaseHTTPRequestHandler):
   def do_GET(self):
     if self.path == "/":
       self.send_response( 200 ); # OK
+
       self.send_header( "Content-type", "text/html" );
       self.send_header( "Content-length", len(inputForm) );
       self.end_headers();
@@ -16,9 +18,37 @@ class Handler(BaseHTTPRequestHandler):
       self.wfile.write( bytes( inputForm, "utf-8" ) );
 
     else:
+      sendReply = False
+      mimetype = ""
+      print(self.path)
+      if self.path.endswith(".css"):
+        mimetype = "text/css"
+        sendReply = True
+      if self.path.endswith(".js"):
+        mimetype = "text/babel"
+        sendReply = True
+      
+      if sendReply == True:
+        f = open("src"+self.path, "r")
+        self.send_response(200)
+        css = ""
+        s = f.readline()
+        while s != "":
+          css += s
+          #print(s)
+          s = f.readline()
+
+        self.send_header("Content-type",mimetype)
+        self.send_header( "Content-length", len(css) );
+        self.end_headers()
+        self.wfile.write(bytes(css, "utf-8"))
+        f.close()
+        return
+      
       self.send_response( 404 );
       self.end_headers();
       self.wfile.write( bytes( "404: not found", "utf-8" ) );
+  
   def do_POST(self):
     if self.path == "/molecule":
       self.send_response( 200 ); # OK
@@ -50,8 +80,39 @@ inputForm = """
 <html>
   <head>
     <title> File Upload </title>
+    <link rel="stylesheet" href="styles.css" />
   </head>
   <body>
+      <div id="root"></div>
+        <script src="https://unpkg.com/react@latest/umd/react.development.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/react-dom@latest/umd/react-dom.development.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/babel-standalone@6.26.0/babel.min.js" crossorigin="anonymous"></script>
+    <script type="text/babel">
+        let createElement = React.createElement
+
+        let rootElement = (
+          <div className='ContactList'>
+            <h1 className='ContactList-title'>Contacts</h1>
+            <div>
+              <div className='Contact'>
+                <div className='Contact-avatar'>JN</div>
+                <span className='Contact-name'>Myron Myron</span>
+                <a href='mailto:mladyjen@uoguleph.ca' className='Contact-link'>mladyjen@uoguelph.ca</a>
+              </div>
+              <div className='Contact'>
+                <div className='Contact-avatar'>DW</div>
+                <span className='Contact-name'>Myron Myron</span>
+                <a href='mailto:dwang11@uoguleph.ca' className='Contact-link'>dwang11@uoguelph.ca</a>
+              </div>
+            </div>
+          </div>
+        )
+
+        // The `ReactDOM` variable is set by the second `<script>` tag
+        // in the above HTML file
+        ReactDOM.render(rootElement, document.getElementById('root'))
+
+    </script>
     <h1> File Upload </h1>
     <form action="molecule" enctype="multipart/form-data" method="post">
       <p>
