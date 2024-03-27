d743dee15feb54f674fa23d7725d9b7f8549694c
DanWang725 <dw.smaug@gmail.com>
2024-03-27 16:09:05 -0400

    improve util files for non deterministic encryption

--- index/CryptoUtils.py
+++ index/CryptoUtils.py
@@ -1,7 +1,15 @@
 import os
 from cryptography.hazmat.primitives.ciphers import aead;
 
-def AESSIVWithNonce(k, data):
-    nonce = os.urandom(16)
+def AESSIVEncryptNonce(k, data, nonceLength = 12):
+    typecastedData = bytes(data, 'utf-8')
+    nonce = os.urandom(nonceLength)
     aessiv = aead.AESSIV(k)
-    return nonce + aessiv.encrypt(data, [nonce])
+    ciphertext = aessiv.encrypt(typecastedData, [nonce])
+    return nonce + ciphertext
+
+def AESSIVDecryptNonce(k, ciphertext, nonceLength = 12):
+    nonce = ciphertext[:nonceLength]
+    data = ciphertext[nonceLength:]
+    aessiv = aead.AESSIV(k)
+    return aessiv.decrypt(data, [nonce])
