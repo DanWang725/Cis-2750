dc288bfb37f58516896f4e38688ce3e80fffa4c8
DanWang725 <dw.smaug@gmail.com>
2024-03-28 13:35:13 -0400

    finished the trapdoor generation

--- index/Trapdoor.py
+++ index/Trapdoor.py
@@ -1,5 +1,6 @@
 import sqlparse
-from CryptoUtils import AESSIVEncryptNonce
+from CryptoUtils import AESSIVEncryptNonce, phiFunction, get_xor
+
 
 def ExtractKeywords(sql):
     # print(sql)
@@ -32,7 +33,10 @@ def ExtractKeywords(sql):
 
 def generateTrapdoor(sql, K):
     keywords = ExtractKeywords("""SELECT * FROM PATIENT WHERE Name='Mary' AND Surname='Grant';""")
-    AESSIVEncryptNonce(K, keywords[0]);
+    AESSIVEncryptNonce(K, keywords[0])
+    pos = keywords[0]
+    kW = phiFunction(K, keywords[0])
+    return (pos, kW)
     
 
 
